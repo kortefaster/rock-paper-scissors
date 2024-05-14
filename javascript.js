668d487de42a133a381676c0cc9e0f5f1a1db7a6
kortefaster <tektro12@gmail.com>
2024-05-14 16:03:37 +0200

    add simple html and javascript
    This will make a simple rock paper scissors game that
    you can play through console

---
+++ javascript.js
@@ -0,0 +1,122 @@
+// console.log("Hello World")
+
+let playerScore = 0;
+let computerScore = 0;
+let currentRound = 0;
+
+function getComputerChoice() {
+    let resultPc;
+    let choicePc = Math.floor(Math.random()*300);
+
+    console.log(choicePc);
+
+    if (choicePc<100) { 
+        resultPc = `rock`;
+        // console.log(resultPc)
+        return resultPc;
+    }
+
+    else if (choicePc>=100 && choicePc<200) {
+        resultPc = 'paper';
+        // console.log(resultPc)
+        return resultPc;
+    }
+    else if (choicePc>=200){
+        resultPc = 'scissors';
+        // console.log(resultPc)
+        return resultPc;
+    };
+
+}
+
+
+function getHumanChoice() {
+let humanSelection = prompt('Choose between rock, paper or scissors', '');
+let newHumanSelection = humanSelection.toLowerCase();
+
+if (newHumanSelection==='paper' || newHumanSelection===`rock` || newHumanSelection===`scissors`) {
+    let finalHumanSelection = newHumanSelection;
+    return finalHumanSelection;
+}
+else {
+    alert('YOU HAVE TO Choose between rock, paper or scissors', '')}
+};
+
+
+
+
+
+function playGame() {
+
+
+    // console.log(humanSelection);
+    // console.log(computerSelection);
+
+    
+/*
+    if (playerScore === 5) {
+        console.log(`player wins all game`)
+    } 
+
+    else if(computerScore === 5) {
+        console.log(`computer wins all game`)
+    }
+
+    if(playerScore<5 || computerScore < 5 || currentRound <10) {
+        */
+
+        function playRound()
+         {    
+            let humanSelection = getHumanChoice();
+            let computerSelection = getComputerChoice();
+            console.log(`player chose: ${humanSelection}`);
+            console.log(`computer chose: ${computerSelection}`);
+            
+            if(humanSelection != `rock` && humanSelection != `paper` && humanSelection != `scissors`){
+                console.log(`YOU HAD TO CHOOSE BETWEEN ROCK, PAPER, AND SCISSORS, THE PROGRAM WILL RESET UNTIL YOU CHOOSE CORRECTLY`)
+                return;
+
+            }
+
+            else if (humanSelection===computerSelection) {
+                console.log(`draw`)
+                currentRound++;
+                console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
+                return;
+            } 
+            else if (humanSelection===`rock` && computerSelection ===`paper` || humanSelection===`paper` && computerSelection === 'scissors' || humanSelection=== `scissors` && computerSelection === `rock`) {
+                console.log(`computer wins this round`)
+                computerScore++;
+                currentRound++;
+                console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
+                return;
+                
+            }
+            else {
+                console.log(`player wins this round`)
+               playerScore++;
+               currentRound++;
+               console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
+               return;
+
+            }
+        };
+
+        playRound();
+        playRound();
+        playRound();
+        playRound();
+        playRound();
+        // console.log(computerScore, playerScore);
+    }
+    
+
+
+playGame();
+    
+
+
+
+
+
+
