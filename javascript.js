80b4c16ec607453b30002a8d2da2e93c5242d8b8
kortefaster <tektro12@gmail.com>
2024-05-16 18:58:06 +0200

    add clickable buttons
    Clicked buttons return console.log saying which one was clicked

--- javascript.js
+++ javascript.js
@@ -30,126 +30,109 @@ function getComputerChoice() {
 
 }
 
+let menu = document.querySelector('#menu');
+menu.addEventListener('click', (event) => {
+    let target = event.target;
+    // console.log(event.target);
+
+switch(target.id){
+
+    case `buttonRock`:
+        console.log(`buttonRock was clicked`);
+        break;
+    case `buttonPaper`:
+        console.log(`buttonPaper was clicked`);
+        break;
+    case `buttonScissors`:
+        console.log(`buttonScissors was clicked`);
+        break;
+
+}
+
+
+});
+
+
+
+
+
+
+
+
+
 
 function getHumanChoice() { 
     let humanSelection = prompt('Choose between rock, paper or scissors', '');
+
     let newHumanSelection = humanSelection.toLowerCase();
 
     if (newHumanSelection==='paper' || newHumanSelection===`rock` || newHumanSelection===`scissors`) {
         let finalHumanSelection = newHumanSelection;
         return finalHumanSelection;
     }
+   
+
     else {
-        alert('YOU HAVE TO Choose between rock, paper or scissors')
+        alert('YOU HAVE TO Choose between rock, paper or scissors');
     }
 };
 
 
 
 
-
-function playGame() {
-
-
-    // console.log(humanSelection);
-    // console.log(computerSelection);
-
-    
-/*
-    if (playerScore === 5) {
-        console.log(`player wins all game`)
-    } 
-
-    else if(computerScore === 5) {
-        console.log(`computer wins all game`)
-    }
-
-    while (playerScore<5 && computerScore < 5 && currentRound <10 && stopGame==0) {
-        */
-
-        function playRound()
-         {    
-            let humanSelection = getHumanChoice();
-            let computerSelection = getComputerChoice();
-            console.log(`player chose: ${humanSelection}`);
-            console.log(`computer chose: ${computerSelection}`);
-            
-            if(humanSelection != `rock` && 
-               humanSelection != `paper` && 
-               humanSelection != `scissors`)
-               {
-                console.log(`YOU HAD TO CHOOSE BETWEEN ROCK, PAPER, AND SCISSORS, THE PROGRAM WILL RESET UNTIL YOU CHOOSE CORRECTLY`)
-                stopGame = 1;
-                return;
-
-            }
-
-            else if (humanSelection===computerSelection) {
-                console.log(`draw`)
-                currentRound++;
-                console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
-                return;
-            } 
-            else if (
-                humanSelection===`rock` && computerSelection === `paper` || 
-                humanSelection===`paper` && computerSelection === 'scissors' || 
-                humanSelection=== `scissors` && computerSelection === `rock`) 
-            {
-                console.log(`computer wins this round`)
-                computerScore++;
-                currentRound++;
-                console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
-                return;
-                
-            }
-            else {
-               console.log(`player wins this round`)
-               playerScore++;
-               currentRound++;
-               console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
-               return;
-
-            }
-        };
-
-        playRound();
-        playRound();
-        playRound();
-        playRound();
-        playRound();
-        // console.log(computerScore, playerScore);
-    }
+function playRound()
+{    
 
 
-
-
-
-playGame();
-
-
-function isPrime(){
-    
-let numberInput;
-
- do {
-    numberInput = prompt(`choose a number`, `0`)
-    for (let index = 2; index <= numberInput/2; ) {
-        if (numberInput%index===0){
-            alert('not prime')
-            break;    
-        }
-        else if(index===Math.floor(numberInput/2)){
-            alert(`prime`);
-            break;
-        }
-        else{
-            index++
-        };
-        
-    }
-} while (numberInput)
-
+   
+   let humanSelection = getHumanChoice();
+   if (humanSelection === null){
+       return;   
 }
+   let computerSelection = getComputerChoice();
+   console.log(`player chose: ${humanSelection}`);
+   console.log(`computer chose: ${computerSelection}`);
+   
+   if(humanSelection != `rock` && 
+      humanSelection != `paper` && 
+      humanSelection != `scissors`)
+      {
+       console.log(`YOU HAD TO CHOOSE BETWEEN ROCK, PAPER, AND SCISSORS, THE PROGRAM WILL RESET UNTIL YOU CHOOSE CORRECTLY`)
+       stopGame = 1;
+       return;
+
+   }
+
+   else if (humanSelection===computerSelection) {
+       console.log(`draw`)
+       currentRound++;
+       console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
+       return;
+   } 
+   else if (
+       humanSelection===`rock` && computerSelection === `paper` || 
+       humanSelection===`paper` && computerSelection === 'scissors' || 
+       humanSelection=== `scissors` && computerSelection === `rock`) 
+   {
+       console.log(`computer wins this round`)
+       computerScore++;
+       currentRound++;
+       console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
+       return;
+       
+   }
+   else {
+      console.log(`player wins this round`)
+      playerScore++;
+      currentRound++;
+      console.log(`Computer Score: ${computerScore}, Player Score: ${playerScore}, Current Round: ${currentRound}`);
+      return;
+
+   }
+};
+// playRound();
+
+
 
 
 
